1a346af3424f3ea45a0d157a6c85024670319909
selimaakid1 <selima.akid@yahoo.com>
2020-03-03 14:56:43 +0100

    Updates

---
+++ precache-manifest.49b18854abb3739e330a4a37230b546e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d85aec158997aa3a02a46dc8e167b725",
+    "url": "/Timer/index.html"
+  },
+  {
+    "revision": "a274b2e13d9a5ad8ba6d",
+    "url": "/Timer/static/css/main.33b27b2f.chunk.css"
+  },
+  {
+    "revision": "2e558e3c60414b99c975",
+    "url": "/Timer/static/js/2.191b61b8.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/Timer/static/js/2.191b61b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a274b2e13d9a5ad8ba6d",
+    "url": "/Timer/static/js/main.48508065.chunk.js"
+  },
+  {
+    "revision": "7702642811436c02480f",
+    "url": "/Timer/static/js/runtime-main.07475ad3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Timer/static/media/logo.5d5d9eef.svg"
+  }
+]);
